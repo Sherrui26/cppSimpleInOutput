6e485cd610337b2719cccb1aadd701489d86ff3f
Sherrui <Sharulaiman98@gmail.com>
2021-08-08 20:32:56 +0800

    simpleInputOutput
    imported from github to pc

---
+++ insert_output.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <conio.h>
+
+#define J 14
+
+int main(void){
+	
+	double summationCase,summationRec,summationDeaths;
+	
+	scanf("%d",&summationCase,printf("enter case: "));
+			
+	printf("summationCase is: %d",summationCase);	
+		
+	return 0;
+}
